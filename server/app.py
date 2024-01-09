539157f612806600110f75abe40a02a22452e562
Johnson Mbeba <john.mbeba@student.moringaschool.com>
2024-01-09 19:44:06 +0300

    Lab completed

--- server/app.py
+++ server/app.py
@@ -17,11 +17,46 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        
+        return make_response(
+            [plant.to_dict() for plant in plants],
+            200
+        )
+        
+    def post(self):
+        new_plant = Plant(
+            name=request.json.get('name'),
+            image=request.json.get('image'),
+            price=request.json.get('price')
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        return make_response(
+            new_plant.to_dict(),
+            201
+        )
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
         
+        if not plant:
+            return make_response(
+                "Plant not found",
+                404
+            )
+            
+        return make_response(
+            plant.to_dict(),
+            200
+        )
+        
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
